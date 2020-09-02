ec3effd64168840cce0d1da63294d08085a48868
Sneha Motwani <snehamotwani11@gmail.com>
2020-09-02 15:22:14 +0530

    Check In

---
+++ Stack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+        }
+        public class Stack
+        {
+            public void Push(object obj)
+            {
+
+            }
+            public object Pop()
+            {
+                return 1;
+            }
+            public void Clear()
+            {
+
+            }
+        }
+    }
+}
