391e9b60424f7f31c0af25cdd271d999aacd626c
Sneha Motwani <snehamotwani11@gmail.com>
2020-09-03 09:54:11 +0530

    Output Pasted in Comments

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -7,11 +7,21 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            /*  Output:
+                Current Stack: [ 1  2  3 ]
+                Object Removed: 3
+                Current Stack: [ 1  2 ]
+                Object Removed: 2
+                Current Stack: [ 1 ]
+                Object Removed: 1
+                Current Stack: []
+             */
             Stack stack = new Stack();
 
             stack.Push(1);
             stack.Push(2);
             stack.Push(3);
+           //stack.Push("");
 
             stack.DisplayStack();
 
