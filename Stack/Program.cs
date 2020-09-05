ac006c28cb307cc50d121745f72e97770971c12f
Sneha Motwani <snehamotwani11@gmail.com>
2020-09-05 21:56:05 +0530

    Changes made as per the Code Review

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -16,12 +16,11 @@ namespace Stack
                 Object Removed: 1
                 Current Stack: []
              */
-            Stack stack = new Stack();
+            StackModel stack = new StackModel();
 
             stack.Push(1);
             stack.Push(2);
             stack.Push(3);
-           //stack.Push("");
 
             stack.DisplayStack();
 
@@ -35,62 +34,6 @@ namespace Stack
             Console.WriteLine();
 
             stack.Clear();
-        }
-        public class Stack
-        {
-            int currentIndex = -1;
-            object obj;
-            List<object> stackList = new List<Object>();
-
-            public void Push(object aObj)
-            {
-                obj = aObj;
-                if(obj != null)
-                {
-                    //Console.WriteLine("Index before pushing : " + currentIndex);
-                    currentIndex++;
-                    stackList.Add(obj);
-                    //Console.WriteLine("Index after pushing : " + currentIndex);
-                }
-                else
-                {
-                    Console.WriteLine("Object is null");
-                }
-            }
-            public object Pop()
-            {
-                obj = stackList[currentIndex];
-                if (stackList.Count == 0)
-                {
-                    Console.WriteLine("List is Empty");
-                }
-                else
-                {
-                    //Console.WriteLine("Value of object: " + obj);
-                    //Console.WriteLine("Index before poping : " + currentIndex);
-                    stackList.Remove(obj);
-                    currentIndex--;
-                    //Console.WriteLine("Index after poping : " + currentIndex);
-                }
-                return obj;
-                
-            }
-            public void Clear()
-            {
-                stackList.Clear();
-            }
-            public void DisplayStack()
-            {
-                //obj = stackList[currentIndex];
-                Console.Write("Current Stack: [");
-
-                for (int i = 0; i < stackList.Count; i++)
-                {
-                    Console.Write($" {stackList[i]} ");
-                }
-                Console.Write("]");
-                Console.WriteLine();
-            }
         }
     }
 }
